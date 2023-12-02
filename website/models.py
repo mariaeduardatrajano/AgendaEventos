e08514184f1acfca9f765ac78f1011d16fe894b0
Suelyane <suelyane.ferreira@academico.ifpb.edu.br>
2023-12-02 17:57:27 -0300

    (feat): configurei o app criei os models e fiz as migração

--- website/models.py
+++ website/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Evento(models.Model):
+  nome = models.CharField(max_length=255)
+  descricao = models.TextField()
+  data_hora = models.DateTimeField()
+  organizadores = models.ManyToManyField('Organizador')
+  participantes = models.ManyToManyField('Participante')
+  local = models.OneToOneField('Local', on_delete=models.CASCADE)
+
+class Organizador(models.Model):
+  nome = models.CharField(max_length=255)
+  idade = models.IntegerField()
+
+class Participante(models.Model):
+  nome = models.CharField(max_length=255)
+  idade = models.IntegerField()
+
+class Local(models.Model):
+  nome = models.CharField(max_length=255)
+  endereco = models.CharField(max_length=255)
+  capacidade = models.IntegerField()
